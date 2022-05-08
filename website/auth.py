884c45ae522e56f29e51f19d32ae8e2d74f33b24
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2022-05-08 18:32:17 +0200

    +

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", text="Testing")
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
@@ -12,4 +15,22 @@ def logout():
 
 @auth.route("/sign-up")
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstname")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Email must be grater than 3 characters", category="error")
+        elif len(firstName) < 2:
+            flash("Firstname must be grater than 1f characters", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match.", category="error")
+        elif len(password1) < 7:
+            flash("Password is to short. Needs at least 7 characters", category="error")
+        else:
+            flash("Account created!", category="success")
+
+
     return render_template("sign_up.html")
